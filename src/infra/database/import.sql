52928bb51b326bf983bc5cd8e6f01a68123215a4
Joel Filho <joelfilho02812@gmail.com>
2024-08-21 22:18:30 -0300

    infra: add clients table and relations in import.sql

--- src/infra/database/import.sql
+++ src/infra/database/import.sql
@@ -1,3 +1,8 @@
+CREATE TABLE clients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
     amount DECIMAL(10, 2) NOT NULL,
@@ -7,15 +12,24 @@ CREATE TABLE transactions (
     card_holder_name VARCHAR(100) NOT NULL,
     card_expiration_date DATE NOT NULL,
     card_cvv VARCHAR(3) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    clientId INTEGER NOT NULL,
+
+    FOREIGN KEY (clientId) REFERENCES clients(id)
 );
 
 CREATE TABLE payables (
     id SERIAL PRIMARY KEY,
-    transaction_id INT REFERENCES transactions(id),
     status VARCHAR(20) CHECK (status IN ('paid', 'waiting_funds')) NOT NULL,
     payment_date DATE NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
     fee DECIMAL(10, 2) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    clientId INTEGER NOT NULL,
+    transactionId INTEGER NOT NULL,
+
+    FOREIGN KEY (clientId) REFERENCES clients(id),
+    FOREIGN KEY (transactionId) REFERENCES transactions(id)
 );
+
+INSERT INTO clients (name) VALUES ('John Doe'), ('Bob Brown'), ('Maria Green');
